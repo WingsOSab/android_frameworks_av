3f09ff340cdd21399b9c4685b319c064883a2647
ShevT <evgeny.shishkov@gmail.com>
2023-09-11 08:59:19 +0300

    av: CCodec: Set Pipeline paused timeout to 1000ms
     This fixes an issue where video initially loop when played
     in Сhrome-like browsers and a connected BT audio device.
    
    Change-Id: I2b5b37131a8df76159b39f44a38abbb8c1003362
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -84,6 +84,10 @@ constexpr size_t kSmoothnessFactor = 4;
 // This is for keeping IGBP's buffer dropping logic in legacy mode other
 // than making it non-blocking. Do not change this value.
 const static size_t kDequeueTimeoutNs = 0;
+// If app goes into background, decoding paused. we have WA logic in HAL to sleep some actions.
+// This value is to monitor if decoding is paused then we can signal a new empty work to HAL
+// after app resume to foreground to notify HAL something
+const static uint64_t kPipelinePausedTimeoutMs = 1000;
 
 static bool areRenderMetricsEnabled() {
     std::string v = GetServerConfigurableFlag("media_native", "render_metrics_enabled", "false");
